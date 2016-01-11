0014824f32a67a3dc9d49b060b0bb94032fc799f
GlovesforATT <Jeffrey@Windows.local>
2016-01-11 15:06:53 -0800

    first commit

---
+++ multiple3and5.js
@@ -0,0 +1,20 @@
+
+  	var i = 1;
+  	var multiple3 = 0;
+  	var multiple5 = 0;
+	while (i < 1000)
+	{
+	  	if ((i % 3) == 0)
+		{
+			console.log(i, ", ");
+			multiple3 += i;
+		}
+		if ((i % 5) == 0)
+		{
+			console.log(i, ", ");
+			multiple5 += i;
+		}
+		var total = multiple3 + multiple5 
+		i++;
+	}
+	console.log("Total is : " + total)
