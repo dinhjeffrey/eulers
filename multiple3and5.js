8e74dcc137d0a850babe28aebb717f2f073eb3b9
GlovesforATT <Jeffrey@Windows.local>
2016-01-11 15:45:08 -0800

    revised with map, reduce, and filter

--- multiple3and5.js
+++ multiple3and5.js
@@ -1,20 +1,28 @@
 
-  	var i = 1;
-  	var multiple3 = 0;
-  	var multiple5 = 0;
-	while (i < 1000)
-	{
-	  	if ((i % 3) == 0)
-		{
-			console.log(i, ", ");
-			multiple3 += i;
-		}
-		else if ((i % 5) == 0)
-		{
-			console.log(i, ", ");
-			multiple5 += i;
-		}
-		var total = multiple3 + multiple5 
-		i++;
-	}
-	console.log("Total is : " + total)
+ //  	var i = 1;
+ //  	var multiple3 = 0;
+ //  	var multiple5 = 0;
+	// while (i < 1000)
+	// {
+	//   	if ((i % 3) == 0)
+	// 	{
+	// 		console.log(i, ", ");
+	// 		multiple3 += i;
+	// 	}
+	// 	else if ((i % 5) == 0)
+	// 	{
+	// 		console.log(i, ", ");
+	// 		multiple5 += i;
+	// 	}
+	// 	var total = multiple3 + multiple5 
+	// 	i++;
+	// }
+	// console.log("Total is : " + total)
+
+
+	 console.log(Array.apply(null, Array(1000)).map(function (_, i) 
+	 {return i+1}).filter(function(val) 
+	{ return val % 3 == 0 || val % 5 == 0 }).reduce(function(a, b) { return a+b }, 0))
+
+// console.log(Array.apply(null, Array(1000)).map(function (_, i) {return i+1}))
+
